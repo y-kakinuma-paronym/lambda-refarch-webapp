eed9fab58adc4f3b682bac4becc749fb251b1e68
y-kakinuma <y-kakinuma@paronym.jp>
2020-12-07 01:08:09 +0900

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "7780g7pa3l6d6t118u3uauej3t",     // CognitoClientID
-  "api_base_url": "https://o4uo6256sa.execute-api.ap-northeast-1.amazonaws.com/prod",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "mytodoappdemo-serverless-app-todo-202012032249.auth.ap-northeast-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d2861nlb6b4r63.amplifyapp.com"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "7m31018rapva3jj5quc2qdv55v",     // CognitoClientID
+  "api_base_url": "https://r5ff8dheji.execute-api.ap-northeast-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-serverless.auth.ap-northeast-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d14yu0p8gd7nex.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
